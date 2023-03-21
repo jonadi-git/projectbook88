10cd4cc44f4921eab7374a8d333f3665020d520a
jonadi-git <115789318+jonadi-git@users.noreply.github.com>
2023-03-21 11:40:23 +0100

    Add files via upload

---
+++ project-book-reeks8-8-main/project-book-reeks8-8-main/src/main/java/be/ucll/reeks88/Reeks88Application.java
@@ -0,0 +1,13 @@
+package be.ucll.reeks88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reeks88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reeks88Application.class, args);
+	}
+
+}
